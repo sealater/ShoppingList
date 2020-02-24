b4d5bfb3af25c94a298840367198f86d29fd1026
sealater <54549335+sealater@users.noreply.github.com>
2020-02-24 15:27:15 -0600

    done

---
+++ ShoppingList/ShoppingList/ShoppingListData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace ShoppingList
+{
+    class ShoppingListData : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        List<string> items = new List<string>();
+
+        public IEnumerable<string> Items => items.ToArray();
+
+        public void Add(string item)
+        {
+            items.Add(item);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Items"));
+        }
+
+        public void Remove(string item)
+        {
+            items.Remove(item);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Items"));
+        }
+    }
+}
---
+++ ShoppingList/ShoppingList/ShoppingListItems.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ShoppingList
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class ShoppingListItems : UserControl
+    {
+        public ShoppingListItems()
+        {
+            InitializeComponent();
+        }
+
+        public void OnRemoveButtonClicked(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is ShoppingListData data)
+            {
+                if (sender is Button button)
+                {
+                    data.Remove(button.DataContext.ToString());
+                }
+            }
+        }
+    }
+}
